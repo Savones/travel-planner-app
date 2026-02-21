e754bd8ac0c3cb2323e31526e0db6974fe350be0
Essi Savonius <essi.savonius@hotmail.fi>
2026-02-21 22:34:15 +0200

    add login controller

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const tripsRouter = require('./controllers/trips')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -11,5 +12,6 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 app.use(express.json())
 app.use('/api/trips', tripsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
