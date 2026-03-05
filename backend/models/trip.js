efbd61981b2aca62202cf7537d029f6a598eecef
Essi Savonius <essi.savonius@hotmail.fi>
2026-03-05 22:04:41 +0200

    replace _id with id in location schema

--- backend/models/trip.js
+++ backend/models/trip.js
@@ -29,4 +29,12 @@ tripSchema.set('toJSON', {
   }
 })
 
+locationSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Trip', tripSchema)
