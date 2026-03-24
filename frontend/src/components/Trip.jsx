2d3ec077386bb5296ab806a413dd29c79f5283ad
Essi Savonius <essi.savonius@hotmail.fi>
2026-03-24 14:10:03 +0200

    add no trip locations text

--- frontend/src/components/Trip.jsx
+++ frontend/src/components/Trip.jsx
@@ -5,9 +5,9 @@ import { deleteTrip } from '../reducers/tripReducer'
 import { showNotification } from '../reducers/notificationReducer'
 
 const Trip = () => {
-  const { id } = useParams()
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const { id } = useParams()
   const trip = useSelector(state =>
     state.trips.find(t => t.id === id)
   )
@@ -41,7 +41,9 @@ const Trip = () => {
 
   return (
     <div className='tripPageDiv'>
+
       <h2>{trip.title}</h2>
+
       <div className='tripDetailsButtons'>
         <button type="button" onClick={editTrip}>
           Edit
@@ -50,8 +52,11 @@ const Trip = () => {
           Delete trip
         </button>
       </div>
+
       <TripSummary trip={trip} />
+
       <div className='locationsDiv'>
+        {trip.locations?.length === 0 && <p>No locations yet. Click "edit" to add a location.</p>}
         {trip.locations && trip.locations.map(location => (
           <div className='locationDiv' style={{ backgroundColor: location.backgroundColor }} key={location.id}>
             <div className='locationTitleDiv'>{location.city}, {location.country}</div>
@@ -61,6 +66,7 @@ const Trip = () => {
           </div>
         ))}
       </div>
+
     </div>
   )
 }
