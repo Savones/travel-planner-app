07c2dccb830c81a6cdb0b332249c0935e49820fe
Essi Savonius <essi.savonius@hotmail.fi>
2026-04-08 17:13:04 +0300

    fix shared filter

--- frontend/src/components/TripList.jsx
+++ frontend/src/components/TripList.jsx
@@ -18,7 +18,7 @@ const TripList = () => {
   const search = useField('search')
 
   const userTrips = trips.filter(trip => trip.user?.id === user.id)
-  const sharedTrips = trips.filter(trip => trip.users?.some(u => u.id === user.id))
+  const sharedTrips = trips.filter(trip => trip.users?.some(u => u.user?.id === user.id))
   const allTrips = [...userTrips, ...sharedTrips]
   const [filters, setFilters] = useState({
     upcoming: false, ongoing: false, shared: false, past: false
