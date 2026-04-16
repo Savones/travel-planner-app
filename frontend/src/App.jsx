c56cff1d1eb6095724530c72696e5faf4993cfd3
Essi Savonius <essi.savonius@hotmail.fi>
2026-04-16 15:01:04 +0300

    fix realoading redirects to login bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import tripService from './services/trips'
 import Trip from './components/Trip'
 import TripList from './components/TripList'
@@ -9,11 +9,7 @@ import SignUpForm from './components/SignUpForm'
 import EditTripForm from './components/EditTripForm'
 import LocationForm from './components/LocationForm'
 import ProtectedRoute from './components/ProtectedRoute'
-import {
-  BrowserRouter as Router,
-  Routes, Route
-} from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUser, clearUser } from './reducers/userReducer'
 import { initializeUsers } from './reducers/usersReducer'
@@ -23,6 +19,7 @@ import Notification from './components/Notification'
 const App = () => {
   const dispatch = useDispatch()
   const user = useSelector(state => state.user)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     dispatch(initializeTrips())
@@ -36,12 +33,17 @@ const App = () => {
       dispatch(setUser(user))
       tripService.setToken(user.token)
     }
+    setLoading(false)
   }, [dispatch])
 
   const handleLogout = () => {
     window.localStorage.removeItem("loggedUser")
     tripService.setToken(null)
     dispatch(clearUser())
+  }
+
+  if (loading) {
+    return <div>Loading...</div>
   }
 
   return (
@@ -55,11 +57,11 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<LoginForm />} />
           <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/trips/:id" element={<ProtectedRoute user={user}><Trip /></ProtectedRoute>} />
-          <Route path="/trips/:id/edit" element={<ProtectedRoute user={user}><EditTripForm /></ProtectedRoute>} />
-          <Route path="/" element={<ProtectedRoute user={user}><TripList /></ProtectedRoute>} />
-          <Route path="/create" element={<ProtectedRoute user={user}><TripForm /></ProtectedRoute>} />
-          <Route path="/trips/:id/addLocation" element={<ProtectedRoute user={user}><LocationForm /></ProtectedRoute>} />
+          <Route path="/trips/:id" element={<ProtectedRoute><Trip /></ProtectedRoute>} />
+          <Route path="/trips/:id/edit" element={<ProtectedRoute><EditTripForm /></ProtectedRoute>} />
+          <Route path="/" element={<ProtectedRoute><TripList /></ProtectedRoute>} />
+          <Route path="/create" element={<ProtectedRoute><TripForm /></ProtectedRoute>} />
+          <Route path="/trips/:id/addLocation" element={<ProtectedRoute><LocationForm /></ProtectedRoute>} />
         </Routes>
       </>
     </Router>
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,12 +1,23 @@
-import { Navigate } from "react-router-dom"
+import { useEffect } from 'react'
 import { useDispatch } from "react-redux"
 import { showNotification } from '../reducers/notificationReducer'
+import { useNavigate } from "react-router-dom"
+import { useSelector } from 'react-redux'
 
-const ProtectedRoute = ({ user, children }) => {
+const ProtectedRoute = ({ children }) => {
+  const user = useSelector(state => state.user)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login')
+      dispatch(showNotification('Redirected to login page.', 5000))
+    }
+  }, [user, navigate])
+
   if (!user) {
-    const dispatch = useDispatch()
-    dispatch(showNotification('Redirected to login page.', 5000))
-    return <Navigate to="/login" />
+    return null
   }
   return children
 }
